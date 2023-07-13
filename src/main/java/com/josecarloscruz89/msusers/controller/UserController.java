5d8540e106866eec0abfa045393d5c0180595e0b
Jose Carlos <zecarlos200516@gmail.com>
2023-07-13 20:28:35 -0300

    Controller

---
+++ src/main/java/com/josecarloscruz89/msusers/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.josecarloscruz89.msusers.controller;
+
+import com.josecarloscruz89.msusers.model.dto.UserResponse;
+import com.josecarloscruz89.msusers.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserResponse> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+}
