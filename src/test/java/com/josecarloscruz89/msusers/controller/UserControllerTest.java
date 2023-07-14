84ddf8d2370c2729b8ee5bb5b4bd2ba5b9b8f64b
Jose Carlos <zecarlos200516@gmail.com>
2023-07-14 20:45:36 -0300

    Unit Test GET

---
+++ src/test/java/com/josecarloscruz89/msusers/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package com.josecarloscruz89.msusers.controller;
+
+import com.josecarloscruz89.msusers.model.dto.UserResponse;
+import com.josecarloscruz89.msusers.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+@ExtendWith(SpringExtension.class)
+@DisplayName("User Controller Tests")
+public class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static final String USERS_ENDPOINT = "/users";
+
+    @Test
+    @DisplayName("Should return a list with all users")
+    public void shouldReturnAListWithAllUsers() throws Exception {
+        List<UserResponse> users = new ArrayList<>();
+
+        String uuid = UUID.randomUUID().toString();
+
+        UserResponse userResponse = UserResponse.builder()
+                .name("Jose")
+                .age(33)
+                .uuid(uuid)
+                .build();
+
+        users.add(userResponse);
+
+        when(userService.getAllUsers())
+                .thenReturn(users);
+
+        mockMvc.perform(get(USERS_ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].uuid", is(uuid)))
+                .andExpect(jsonPath("$[0].name", is("Jose")))
+                .andExpect(jsonPath("$[0].age", is(33)));
+
+        verify(userService, times(1)).getAllUsers();
+        verifyNoMoreInteractions(userService);
+    }
+}
---
+++ src/test/java/com/josecarloscruz89/msusers/service/UserServiceTest.java
@@ -0,0 +1,95 @@
+package com.josecarloscruz89.msusers.service;
+
+import com.josecarloscruz89.msusers.model.dto.UserResponse;
+import com.josecarloscruz89.msusers.model.entity.UserEntity;
+import com.josecarloscruz89.msusers.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("User Service Tests")
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private final List<UserEntity> entities = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        UserEntity userOne = UserEntity.builder()
+                .age(20)
+                .name("Bob")
+                .uuid(UUID.randomUUID().toString())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        UserEntity userTwo = UserEntity.builder()
+                .age(30)
+                .name("John")
+                .uuid(UUID.randomUUID().toString())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        entities.add(userOne);
+        entities.add(userTwo);
+    }
+
+    @Test
+    @DisplayName("Should return a list with all users")
+    public void shouldReturnAListWithAllUsers() {
+        //given
+        given(userRepository.findAll())
+                .willReturn(entities);
+
+        List<UserResponse> entitiesConverted = entities.stream()
+                .map(UserResponse::valueOf)
+                .collect(Collectors.toList());
+
+        //when
+        List<UserResponse> userResponses = userService.getAllUsers();
+
+        //then
+        assertEquals(entitiesConverted, userResponses);
+
+        verify(userRepository, times(1)).findAll();
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    @DisplayName("Should return an empty list")
+    public void shouldReturnAnEmptyList() {
+        given(userRepository.findAll())
+                .willReturn(Collections.emptyList());
+
+        List<UserResponse> userResponses = userService.getAllUsers();
+
+        assertEquals(userResponses.size(), 0);
+
+        verify(userRepository, times(1)).findAll();
+        verifyNoMoreInteractions(userRepository);
+    }
+}
