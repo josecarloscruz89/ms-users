12e0c0a7cdbc11e3fa3c75d0b09144fd9e726b2a
Jose Carlos <zecarlos200516@gmail.com>
2023-07-20 19:22:21 -0300

    Integration Test GET userId

--- src/test/java/com/josecarloscruz89/msusers/integration/UserControllerTestIT.java
+++ src/test/java/com/josecarloscruz89/msusers/integration/UserControllerTestIT.java
@@ -10,9 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static com.josecarloscruz89.msusers.integration.factory.UserFactory.createUser;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasLength;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.jdbc.JdbcTestUtils.deleteFromTables;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,6 +35,8 @@ public class UserControllerTestIT {
     private JdbcTemplate jdbcTemplate;
 
     private static final String USERS_ENDPOINT = "/users";
+    private static final String USERS_BY_ID_ENDPOINT = "/users/{userId}";
+    private static final Integer UUID_LENGTH = UUID.randomUUID().toString().length();
 
     @BeforeEach
     public void setup() {
@@ -52,5 +58,31 @@ public class UserControllerTestIT {
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[*].name", containsInAnyOrder("userTestOne", "userTestTwo")))
                 .andExpect(jsonPath("$[*].age", containsInAnyOrder(30, 40)));
+    }
+
+    @Test
+    @DisplayName("Should return an user by id")
+    void shouldReturnAnUserById() throws Exception {
+        UserEntity userTestGetUserById = createUser("userTestGetUserById", 15);
+
+        userRepository.save(userTestGetUserById);
+
+        String uuid = userTestGetUserById.getUuid();
+
+        mockMvc.perform(get(USERS_BY_ID_ENDPOINT, uuid))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("userTestGetUserById")))
+                .andExpect(jsonPath("$.age", is(15)))
+                .andExpect(jsonPath("$.uuid", is(uuid)))
+                .andExpect(jsonPath("$.uuid", hasLength(uuid.length())));
+    }
+
+    @Test
+    @DisplayName("Should return 404 NotFound Exception when the user does not exist")
+    void shouldReturn404NotFoundExceptionWhenTheUserDoesNotExist() throws Exception {
+        String uuid = "999";
+
+        mockMvc.perform(get(USERS_BY_ID_ENDPOINT, uuid))
+                .andExpect(status().isNotFound());
     }
 }
